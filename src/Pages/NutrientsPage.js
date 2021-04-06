9538046f04efa8bb15f1feaf86e62ff6495b3866
Raphaela Dohrmann <raphaela.dohrmann@7p-group.com>
2021-04-06 13:23:29 +0200

    added Link for Book and img alt

--- src/Pages/NutrientsPage.js
+++ src/Pages/NutrientsPage.js
@@ -18,12 +18,12 @@ const NutrientsPage = () => {
         <Container id="page-container">
             <Jumbotron >
                 <Header title={headerItems.title} text={headerItems.text} />
-                <button className="custom-btn"><a href="#">Buchempfehlung</a></button>
+                <button className="custom-btn"><a href="https://www.nikorittenau.com/vegan-klischee-ade/" target="_blank" rel="noreferrer">Buchempfehlung</a></button>
             </Jumbotron>
             <section className="nutrients-intro">
                 <p>Trotz vielfältiger Hinweise auf gesundheitliche Vorteile ist eine vegane Ernährung ist nicht automatisch gleichzusetzen mit einer "gesunden" Ernährung. Gesund wird auch diese Ernährungsform erst, wenn sie hauptsächlich auf unverarbeiteten Lebensmitteln basiert, vielfältig ist und eine ausgewogene Nährstoffbilanz bereitstellt. Obwohl eine rein pflanzliche Ernährung generell viele Nährstoffe enthält, gibt es aufgrund des Ausschlusses tierischer Lebensmittel ein paar Nährstoffe, die besondere Beachtung finden sollten, da ein Mangel an ihnen schwerwiegende Folgen für die Gesundheit und Leistungsfähigkeit haben kann.</p>
                 <hr />
-                <img className="nutrients-pyramid" src={pyramid}></img>
+                <img className="nutrients-pyramid" src={pyramid} alt="Übersicht über die vegane Ernährungspyramide" />
                 <hr />
             </section>
             <section className="criticals">
